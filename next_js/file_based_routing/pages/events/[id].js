827b5ff0973c09acd40a9ff1ef20022577c93f5e
Marcel Czuryszkiewicz <cczuryszkiewicz@gmail.com>
2022-11-28 17:12:30 +0100

    more components

--- next_js/file_based_routing/pages/events/[id].js
+++ next_js/file_based_routing/pages/events/[id].js
@@ -1,5 +1,10 @@
 import {useRouter} from 'next/router';
 import { getEventById } from '../../dummy-data';
+import { Fragment } from 'react';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 
 
 function IndividualEventPage(){
@@ -13,7 +18,14 @@ function IndividualEventPage(){
     }
     
     return (
-            <div><h1>Individual Event page</h1></div>
+        <Fragment>
+            <EventSummary title={event.title}/>
+            <EventLogistics date={event.date} addres={event.location} image={event.image} imageAlt={event.title}/>
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+
+        </Fragment>
     );
 }
 
