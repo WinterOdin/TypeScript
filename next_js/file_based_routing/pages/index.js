9f870e6914aec539b027f2f9224fe8fb6d904e6f
Marcel Czuryszkiewicz <cczuryszkiewicz@gmail.com>
2022-12-04 12:16:29 +0100

    moved to database

--- next_js/file_based_routing/pages/index.js
+++ next_js/file_based_routing/pages/index.js
@@ -1,20 +1,30 @@
-import {getFeaturedEvents} from '../dummy-data';
+import {getFeaturedEvents} from '../helpers/api-utils';
 import EventList from '../components/events/event-list';
 
-function HomePage(){
+function HomePage(props){
 
-    const featuredData = getFeaturedEvents();
+    
     return (
         
         <div>
             <h1>Featured Events</h1>
 
             <div>
-                <EventList items={featuredData}/>
+                <EventList items={props.featuredEvents}/>
             </div>
         </div>
 
     );
 }
+
+export async function getStaticProps(){
+    const featuredEvents = await getFeaturedEvents()
+    return {
+        props: {
+            featuredEvents:featuredEvents
+        }
+    }
+}
+
 
 export default HomePage;
